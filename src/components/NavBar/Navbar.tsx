ab6d5597b3e9ce5a3fb0fc95f7fd5afeaa19bf32
DimaShumanskiy <dimaihfn@gmail.com>
2021-11-11 16:06:03 +0300

    nav link users

--- src/components/NavBar/Navbar.tsx
+++ src/components/NavBar/Navbar.tsx
@@ -29,6 +29,10 @@ function Navbar(props: NavbarPopsType) {
             <div className={s.item}>
                 <NavLink to="/settings" activeClassName={s.activeLink}>Settings</NavLink>
             </div>
+             <div className={s.item}>
+                <NavLink to="/users" activeClassName={s.activeLink}>Users</NavLink>
+            </div>
+
             <div>
                 <h3>Friends</h3>
                 <article className={s.friendsItem}>
