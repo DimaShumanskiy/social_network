1972b69132fdebf5ba03e7b597d579e0b86fa21b
DimaShumanskiy <dimaihfn@gmail.com>
2021-11-13 12:12:16 +0300

    componentDidMount

--- src/components/Users/Users.tsx
+++ src/components/Users/Users.tsx
@@ -14,13 +14,16 @@ type UsersPropsType = {
 
 class Users extends React.Component<UsersPropsType> {
 
-    constructor(props: UsersPropsType) {
-        super(props);
-        axios.get("https://social-network.samuraijs.com/api/1.0/users")
+    // constructor(props: UsersPropsType) {
+    //     super(props);
+
+    componentDidMount() {
+            axios.get("https://social-network.samuraijs.com/api/1.0/users")
                 .then(response => {
                     this.props.setUsers(response.data.items)
                 })
-    }
+        }
+
 
     render() {
         return (
