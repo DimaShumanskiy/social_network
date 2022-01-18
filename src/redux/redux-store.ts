e3e33e283027ef0d57e61e778c9c2eca14fe166e
DimaShumanskiy <dimaihfn@gmail.com>
2022-01-18 13:40:03 +0300

    chrome extensions для react и redux

--- src/redux/redux-store.ts
+++ src/redux/redux-store.ts
@@ -1,10 +1,10 @@
-import {applyMiddleware, combineReducers, createStore} from "redux";
+import {applyMiddleware, combineReducers, compose, createStore} from "redux";
 import profileReducer from "./profileReducer";
 import dialogsReducer from "./dialogsReducer";
 import sideBarReducer from "./sideBarReducer";
 import usersReducer from "./usersReducer";
 import authReducer from "./authReducer";
-import { reducer as formReducer } from 'redux-form'
+import {reducer as formReducer} from 'redux-form'
 import thunkMiddleware from "redux-thunk"
 import appReducer from "./appReducer";  // санковый Middleware - это не thunk - это промежуточный уровень
 //который внедряем в наш стор
@@ -17,11 +17,15 @@ let reducers = combineReducers({
     app:appReducer,
     form: formReducer,
 })
+//@ts-ignore
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose; // для расширения хрома
+
+export const store = createStore(reducers,  composeEnhancers(applyMiddleware(thunkMiddleware)));
 
 export type rootReducerType = ReturnType<typeof reducers>
-export const store = createStore(reducers, applyMiddleware(thunkMiddleware))
+// export const store = createStore(reducers, applyMiddleware(thunkMiddleware))
 
 //@ts-ignore
-window.store = store
+window.__store__ = store
 
 export type AppStateType = typeof store;
